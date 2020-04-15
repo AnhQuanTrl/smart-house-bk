08dd673c2105936f9e0606cf946a10c2b1933572
AnhQuan <anhquan.tranle0601@gmail.com>
2020-04-15 22:12:46 +0700

    Initiate Project.

---
+++ src/main/java/com/salt/smarthomebackend/SmartHomeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.salt.smarthomebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeBackendApplication.class, args);
+	}
+
+}
