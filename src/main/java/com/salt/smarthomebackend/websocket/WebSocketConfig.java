d65ff575a84c71cb53bf3a53d76db573c5b6b26a
AnhQuan <anhquan.tranle0601@gmail.com>
2020-04-18 12:35:10 +0700

    fix websocket implementation. also testable on web client.

--- src/main/java/com/salt/smarthomebackend/websocket/WebSocketConfig.java
+++ src/main/java/com/salt/smarthomebackend/websocket/WebSocketConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+        registry.addEndpoint("/chat-websocket").withSockJS();
     }
 
     @Override
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,52 @@
+var stompClient = null;
+
+function resetProperties(connected) {
+    $("#button-start").prop("disabled", connected);
+    $("#button-stop").prop("disabled", !connected);
+    if (connected) {
+        $("#input-group").show();
+        $("#infotext").html("You are connected");
+    }
+    else {
+        $("#input-group").hide();
+        $("#infotext").html("You are not connected");
+    }
+
+}
+
+function connect() {
+    var socket = new SockJS('/chat-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        resetProperties(true);
+        console.log("reset...");
+        stompClient.subscribe('/topic/result', function (greeting) {
+            showMessageOnPage(greeting.body);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    resetProperties(false);
+}
+
+function sendMessage() {
+    stompClient.send("/app/sensors", {}, JSON.stringify({'message': $("#message-input").val()}));
+}
+
+function showMessageOnPage(message) {
+    $("#message").append("" + message + "");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    resetProperties(false);
+    $( "#button-start" ).click(function() { connect(); });
+    $( "#button-stop" ).click(function() { disconnect(); });
+    $( "#button-send" ).click(function() { sendMessage(); });
+});
