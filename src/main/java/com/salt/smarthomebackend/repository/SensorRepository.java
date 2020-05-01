0f5476dd83258b079e4b1b446f8b739ee15c7c6c
fouriv <ivy404aprilian@gmail.com>
2020-05-01 22:26:45 +0700

    chande JPArepo to CRUD repo

--- src/main/java/com/salt/smarthomebackend/repository/SensorRepository.java
+++ src/main/java/com/salt/smarthomebackend/repository/SensorRepository.java
@@ -1,8 +1,11 @@
 package com.salt.smarthomebackend.repository;
 
 import com.salt.smarthomebackend.model.Sensor;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface SensorRepository extends JpaRepository<Sensor, Long> {
-    Sensor findByName(String name);
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface SensorRepository extends CrudRepository<Sensor, Integer> {
+
 }
