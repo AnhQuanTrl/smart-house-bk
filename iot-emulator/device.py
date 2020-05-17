bdd757fe41a050e91cd91bafbf578778090bef79
AnhQuan <anhquan.tranle0601@gmail.com>
2020-05-18 01:45:47 +0700

    add some emulator feature

---
+++ iot-emulator/device.py
@@ -0,0 +1,22 @@
+class LightSensor:
+    def __init__(self, id):
+        self.id = id
+        self.light = 0
+
+    def change_light(self):
+        self.light = (self.light + 100) % 1024
+
+    def __str__(self):
+        return '{{id={}, light={}}}'.format(self.id, self.light)
+
+
+class Light:
+    def __init__(self, id):
+        self.mode = False
+        self.id = id
+
+    def toggle(self, value):
+        self.mode = value
+
+    def __str__(self):
+        return '{{id={}, mode={}}}'.format(self.id, "ON" if self.mode else "OFF")
--- iot-emulator/emulator.py
+++ iot-emulator/emulator.py
@@ -1,6 +1,9 @@
 import paho.mqtt.client as mqtt
 import time
+from device import *
 broker_address = 'localhost'
+light_sensor = LightSensor(1)
+light = Light(5)
 
 
 def on_connect(client, userdata, flags, rc):
@@ -8,7 +11,13 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    if msg.topic == 'topic/app':
+        command = msg.payload.decode('UTF-8')
+        print(command)
+        if command == "ON":
+            light.toggle(True)
+        elif command == "OFF":
+            light.toggle(False)
 
 
 client = mqtt.Client()
@@ -17,5 +26,5 @@ client.on_message = on_message
 client.connect(broker_address, 1883, 60)
 client.loop_start()
 while True:
-    client.publish('topic/light', 'Hey')
+    client.publish('topic/light', str(light_sensor) + " " + str(light))
     time.sleep(10)
