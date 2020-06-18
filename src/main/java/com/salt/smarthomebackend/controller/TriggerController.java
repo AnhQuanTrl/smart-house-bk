8a592e7e6e222161c1d46184dfea4268c1d07905
fouriv <ivy404aprilian@gmail.com>
2020-06-18 13:13:30 +0700

    Fetch all triggers

--- src/main/java/com/salt/smarthomebackend/controller/TriggerController.java
+++ src/main/java/com/salt/smarthomebackend/controller/TriggerController.java
@@ -1,6 +1,7 @@
 package com.salt.smarthomebackend.controller;
 
 
+import com.salt.smarthomebackend.model.Device;
 import com.salt.smarthomebackend.model.LightBulb;
 import com.salt.smarthomebackend.model.LightSensor;
 import com.salt.smarthomebackend.model.Trigger;
@@ -31,12 +32,33 @@ public class TriggerController {
         this.triggerRepository = triggerRepositoryy;
     }
 
+    @GetMapping(value = "/{sensorId}")
+    public List<Trigger> all(@PathVariable Long sensorId, @AuthenticationPrincipal ClientPrincipal clientPrincipal) {
+        Long usr_id = clientPrincipal.getId();
+        List<Trigger> triggerLst = new ArrayList<>();
+
+        for (Trigger trigger : triggerRepository.findAll()) {
+            System.out.println(trigger.getId());
+            System.out.println(trigger.getLightSensor().getClient().getId());
+            System.out.println(usr_id);
+            System.out.println(trigger.getLightSensor().getId());
+            System.out.println(sensorId);
+            if (trigger.getLightSensor().getClient() != null && trigger.getLightSensor().getClient().getId().equals(usr_id)) {
+                if (trigger.getLightSensor().getId().equals(sensorId)) {
+                    System.out.println("hhhh");
+                    triggerLst.add(trigger);
+                }
+            }
+        }
+        return triggerLst;
+    }
+
     @PostMapping(value = "/setting")
     public ResponseEntity<?> setTrigger(@RequestBody TriggerRequest triggerRequest, @AuthenticationPrincipal ClientPrincipal clientPrincipal) {
 
             Optional<LightBulb> device = lightBulbRepository.findByName(triggerRequest.getDeviceName());
             Optional<LightSensor> sensor = lightSensorRepository.findByName(triggerRequest.getSensorName());
-            if(device.isPresent() && sensor.isPresent()) {
+            if(device.isPresent() && sensor.isPresent() && device.get().getClient() != null && sensor.get().getClient() != null) {
                 if (clientPrincipal.getId().equals(device.get().getClient().getId()) && clientPrincipal.getId().equals(sensor.get().getClient().getId())) {
                     Trigger trigger = new Trigger(sensor.get(),
                             triggerRequest.getTriggerValue(), device.get(), triggerRequest.getMode());
